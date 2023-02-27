74245080ed4d18c71dbb73b644595a6549d6c44c
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-27 16:44:03 -0300

    Add. os arquivos necessárias para o desenvolvimento

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(() => {
+  hljs.highlightAll();
+
+  document.querySelectorAll('[data-recarrega-pagina]').forEach(botao => {
+    botao.addEventListener('click', () => {
+      window.location.reload;
+    })
+  })
+
+})();
