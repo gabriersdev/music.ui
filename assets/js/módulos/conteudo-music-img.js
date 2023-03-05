b146aff8eaff7180e0c719476214f41fd81c357a
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-03-05 12:01:26 -0300

    Trocando forma de inserção do botão que aciona o modal de preview

--- assets/js/módulos/conteudo-music-img.js
+++ assets/js/módulos/conteudo-music-img.js
@@ -196,8 +196,4 @@ O verso da música aparecerá aqui
 </div>
 </section>
 </main>
-
-<button data-toggle="tooltip" data-acao="exibir-modal-preview" data-bs-custom-class="custom-tooltip" data-placement="left" title="Preview" class="preview-botao">
-<i class="bi bi-eye-fill"></i>
-</button>
 `;
