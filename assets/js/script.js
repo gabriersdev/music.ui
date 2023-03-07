3d895d14403f5ac0891d060aee5fe4e353c85110
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-03-07 16:18:46 -0300

    Corrigindo bug de parâmetro vazio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,27 @@
 "use strict";
 
-import { atualizarDatas, controleFechamentoModal } from './módulos/utilitarios.js';
+import { atualizarDatas, controleFechamentoModal, isEmpty } from './módulos/utilitarios.js';
 import { escutaClickInteracaoModalCompartilha } from './módulos/modal.js';
 import { acionarFuncoesMusicIMG, acionarFuncoesMusicTXT, escutaSelecaoAPP } from './módulos/funcoes.js';
 
 (() => {
-  const param = new URLSearchParams(window.location.search).get('app');
-  switch(param.toLowerCase().trim()){
-    case 'music-txt':
-      acionarFuncoesMusicTXT();
-    break;
-    case 'music-img':
-      acionarFuncoesMusicIMG();
-    break;
-    default:
-      console.log('Não foi definida ação para o parâmetro informado.');
-    break;
+  try{
+    const param = new URLSearchParams(window.location.search).get('app');
+    if(!isEmpty(param)){
+      switch(param.toLowerCase().trim()){
+        case 'music-txt':
+          acionarFuncoesMusicTXT();
+        break;
+        case 'music-img':
+          acionarFuncoesMusicIMG();
+        break;
+        default:
+          console.log('Não foi definida ação para o parâmetro informado.');
+        break;
+      }
+    }
+  }catch(error){
+    console.log('Ocorreu um erro.', error);
   }
 })();
 
